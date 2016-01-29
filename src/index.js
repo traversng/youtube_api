be975e7b855847f2518c35aced3c97e923906fd3
Travis Ueki <travis.ueki@learningfuze.com>
2016-01-28 19:12:21 -0800

    installed lodash to throttle searching on change via .debounce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -33,9 +34,10 @@ class App extends Component {
     }
 //videos in the VideoList component is a prop from the parent in this case its app
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return (
         <div>
-            <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+            <SearchBar onSearchTermChange={videoSearch}/>
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList
                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
